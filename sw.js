862661bd643693fb6b0a96f130748459393bfabc
wellbi1112 <seongjin.yi@gmail.com>
2026-09-15 09:42:19 +0000

    Add LIVE conditions and Google Maps

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 오프라인 캐시 — 요세미티처럼 통신이 약한 곳에서도 페이지가 열립니다 */
-const CACHE='trip2026-v1';
+const CACHE='trip2026-v2';
 const ASSETS=['./','./index.html','./manifest.webmanifest',
   './icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
